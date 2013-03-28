30ad80af5d37d9e4b5d17ea10e9d600eae4ba6d6
Marco Meschini <marco@futureworkshops.com>
2013-03-28 17:35:10 +0000

    Update to latest Ristretto UI

--- FWTPopoverView_Test/FWTPopoverView_Test/AppDelegate.m
+++ FWTPopoverView_Test/FWTPopoverView_Test/AppDelegate.m
@@ -22,37 +22,18 @@
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     self.window.backgroundColor = [UIColor whiteColor];
 
-    RistrettoTableViewController *rootViewController = [[[RistrettoTableViewController alloc] init] autorelease];
     ViewController *defaultVC = [[[ViewController alloc] init] autorelease];
     ViewController *manyVC = [[[ViewController alloc] init] autorelease];
     manyVC.manyPopoversEnabled = YES;
     
-//    NSArray *samples = @[[RistrettoSampleDescriptor descriptorWithTitle:@"Default" className:@"ViewController"],
-//                         ];
     NSArray *samples = @[[RistrettoSampleDescriptor descriptorWithTitle:@"Default" instance:defaultVC],
                          [RistrettoSampleDescriptor descriptorWithTitle:@"Custom - Multiples" instance:manyVC],
                          ];
+    
+    RistrettoTableViewController *rootViewController = [[[RistrettoTableViewController alloc] init] autorelease];
     rootViewController.items = samples;
-    rootViewController.title = @"FWTPopover";
-    
-    //
-    UIView *headerView = [[[UIView alloc] initWithFrame:(CGRect){.0f, .0f, 240.0f, 200.0f}] autorelease];
-    [rootViewController setTableHeaderView:headerView insets:(UIEdgeInsets){58.0f, 40.0f, 45.0f, 40.0f}];
-    
-    UIImageView *iconView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon_popover.png"]] autorelease];
-    iconView.center = CGPointMake(CGRectGetMidX(headerView.bounds), iconView.center.y);
-    [headerView addSubview:iconView];
-    
-    UILabel *label = [[[UILabel alloc] init] autorelease];
-    label.text = @"Choose a sample:";
-    label.font = [UIFont Ristretto_lightFontOfSize:18.0f];
-    label.textColor = [UIColor Ristretto_darkGrayColor];
-    [label sizeToFit];
-    label.center = CGPointMake(CGRectGetMidX(headerView.bounds), CGRectGetHeight(headerView.bounds)-CGRectGetHeight(label.bounds)*.5f);
-    [headerView addSubview:label];
-    
-    self.window.rootViewController = [UINavigationController Ristretto_navigationControllerWithRootViewController:rootViewController];
-    
+    self.window.rootViewController = [UINavigationController Ristretto_navigationControllerWithRootViewController:rootViewController
+                                                                                             defaultHeaderEnabled:YES];
     [self.window makeKeyAndVisible];
     return YES;
 }
